69b602188f9f7ac93ea8cdf11bb137b6e1e4d4f5
tomtomturbo <thomasreist@gmail.com>
2022-05-16 14:57:17 +0200

    new project 1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <body>
+    
+    <div id="app">
+      <div class="header">
+      <h1 style="text-align: left">Welcome to Opportunity</h1>
+      <p class="currentTime">{{ currentDateTime() }}</p>
+    </div>
+      <ul>
+        <li class="li">
+          <span class="entry-daytime">12.00 Uhr</span>
+          <h3>Basisbeschäftigung Besuch</h3>
+          <span class="entry-description">erster Eintrag </span>
+        </li>
+        <li class="li">
+          <span class="entry-daytime">14.00 Uhr </span>
+          <h3>Basisbeschäftigung Besuch 2</h3>
+            <span class="entry-description">nächster Eintrag </span>
+        </li>
+        <li class="li">
+          <span class="entry-daytime">16.00 Uhr</span>
+          <h3>Basisbeschäftigung Besuch X</h3>
+          <span class="entry-description">letzter Eintrag</span>
+        </li>
+      </ul>
+
+    <footer>
+<img class="img" src="./assets/STZH_SEB_Logo.png"
+/>
+<img class="img" src="./assets/Opportunity.png"
+/>
+<img class="img" src="./assets/SAG_Logo_De.png"
+/>
+
+    </footer>
+
+  
+    </div>
+  </body>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  methods: {
+    currentDateTime() {
+      const current = new Date();
+      const date =
+        current.getDate() +
+        "." +
+        (current.getMonth() + 1) +
+        "." +
+        current.getFullYear();
+      return date;
+    },
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap");
+body {
+  margin: 0;
+  font-family: 'Inter', Helvetica, sans-serif;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+  font-size: 28px;
+  line-height: 36px;
+
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.li {
+  list-style-type: none;
+  background-color: #0F05A0;
+  color:#ffbfab;
+  margin: 2rem;
+  padding: 2rem;
+ 
+}
+.entry-daytime {
+  color: #EB5E00;
+  font-weight: 900;
+  font-size: 28px;
+  line-height: 10px;
+
+}
+.header {
+ 
+  margin-left: 4.5rem;
+  margin-right: 2rem;
+  text-align: left;
+  font-size: small;
+
+  }
+
+.currentTime{
+font-size: 28px;
+font-weight: 900;
+}
+.img{
+  width: 20%;
 }
 </style>
